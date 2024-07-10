ef2c3fc6252867676ee350fabc04508d2bc6e4bd
Naoya Kohda <naoya.kohda@gmail.com>
2024-07-11 01:46:26 +0900

    ErrorView 修正

--- test/live_babiniku_web/views/error_view_test.exs
+++ test/live_babiniku_web/views/error_view_test.exs
@@ -2,13 +2,13 @@ defmodule LiveBabinikuWeb.ErrorViewTest do
   use LiveBabinikuWeb.ConnCase, async: true
 
   # Bring render/3 and render_to_string/3 for testing custom views
-  import Phoenix.View
+  import Phoenix.Template
 
   test "renders 404.html" do
-    assert render_to_string(LiveBabinikuWeb.ErrorView, "404.html", []) == "Not Found"
+    assert render_to_string(LiveBabinikuWeb.ErrorView, "404.html", "html", []) == "Not Found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(LiveBabinikuWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(LiveBabinikuWeb.ErrorView, "500.html", "html", []) == "Internal Server Error"
   end
 end
