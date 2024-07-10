16ee428245f3aa61afda1a1013eadabb4681abac
Naoya Kohda <naoya.kohda@gmail.com>
2024-07-11 00:40:12 +0900

    LiveView を 0.18.18 へアップデート

--- lib/live_babiniku_web.ex
+++ lib/live_babiniku_web.ex
@@ -87,12 +87,7 @@ defmodule LiveBabinikuWeb do
     quote do
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
-
-      # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
-      import Phoenix.Component
-
-      # Import basic rendering functionality (render, render_layout, etc)
-      import Phoenix.View
+      use Phoenix.Component
 
       import LiveBabinikuWeb.ErrorHelpers
       import LiveBabinikuWeb.Gettext
